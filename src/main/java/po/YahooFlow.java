d5004cb5f43408e8fdeea021f2344e14e11c7c09
Robert162001 <badalutarobert@gmail.com>
2021-11-23 18:45:27 +0200

    Initialize project

---
+++ src/main/java/po/YahooFlow.java
@@ -0,0 +1,14 @@
+package po;
+
+import org.openqa.selenium.WebDriver;
+
+public class YahooFlow extends BasePage{
+
+    private final WebDriver webDriver;
+
+    public YahooFlow(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+
+}
